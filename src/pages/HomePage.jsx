c8f28c7eab01f902ec22df55da8b0c28df917671
Raynaldo Divian <raymond4114p@gmail.com>
2026-07-07 22:45:42 +0700

    feat: sambungkan halaman publik ke Supabase

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,10 @@ import RoomSection from '../components/public/RoomSection'
 import FacilitySection from '../components/public/FacilitySection'
 import TestimonialSection from '../components/public/TestimonialSection'
 import ContactSection from '../components/public/ContactSection'
+import { useRooms } from '../hooks/useRooms'
+import { useFacilities } from '../hooks/useFacilities'
+import { useTestimonials } from '../hooks/useTestimonials'
+import { useSettings } from '../hooks/useSettings'
 
 export default function HomePage() {
   return (
